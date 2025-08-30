81af68ea24b162555612ab810c987009b0b0b95b
Manish <mks25022025@gmail.com>
2025-08-30 16:32:21 +0530

    7

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 
 const app = express()
-app.use(express.json()) // this is an important line to do a post request
+app.use(express.json()) // this is an important line to do a post request //
 
-// Initializing Environement variable
+// Initializing Environement variable //
 const port = process.env.PORT || 3000
 const uri = process.env.URI
 
-// Connecting backend with database
+// Connecting backend with database //
 mongoose.connect(uri).then(() => {
     console.log("Connection Succesful")
 }).catch((err) => {
     console.error("Failed to connect:", err)
 })
 
-const TestSchema = new mongoose.Schema({}, { strict: false })
-const TestModel = mongoose.models.Test || mongoose.model("Test", TestSchema)
+// const TestSchema = new mongoose.Schema({}, { strict: false })
+// const TestModel = mongoose.models.Test || mongoose.model("Test", TestSchema)
 
 const db = mongoose.connection
 
@@ -29,7 +29,8 @@ app.post('/', async (req, res) => {
     const data = req.body
     console.log("New object:", data)
     try {
-        const newDoc = await TestModel.create(data)
+        // const newDoc = await TestModel.create(data)
+        await db.collection('tests').insertOne(data)
         res.json({ message: "Inserted successfully", newDoc })
     } catch (error) {
         console.error("Insert error:", error)
