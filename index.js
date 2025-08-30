ff063a34f5dab812edcf231ea04c79db712388e9
Manish <mks25022025@gmail.com>
2025-08-30 16:33:54 +0530

    8

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ mongoose.connect(uri).then(() => {
     console.error("Failed to connect:", err)
 })
 
-// const TestSchema = new mongoose.Schema({}, { strict: false })
-// const TestModel = mongoose.models.Test || mongoose.model("Test", TestSchema)
+const TestSchema = new mongoose.Schema({}, { strict: false })
+const TestModel = mongoose.models.Test || mongoose.model("Test", TestSchema)
 
 const db = mongoose.connection
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World4!</h1>')
+    res.send('<h1>Hello World8!</h1>')
 })
 
 app.post('/', async (req, res) => {
     const data = req.body
     console.log("New object:", data)
     try {
-        // const newDoc = await TestModel.create(data)
-        await db.collection('tests').insertOne(data)
+        const newDoc = await TestModel.create(data)
+        // await db.collection('tests').insertOne(data)
         res.json({ message: "Inserted successfully", newDoc })
     } catch (error) {
         console.error("Insert error:", error)
